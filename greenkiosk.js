f0ced757c9576cd06a854562d60cf6cc191ab237
Bright Joy Karanja <brightjoykaranja@gmail.com>
2025-06-05 08:52:01 +0300

    add some images and styling

--- greenkiosk.js
+++ greenkiosk.js
@@ -10,6 +10,8 @@ fruList.style.listStyleType = 'none';
 fruList.style.fontSize = '20px';
 fruList.style.display = 'flex';
 fruList.style.gap = '20px';
+fruList.style.alignItems = 'center'
+fruList.style.justifyContent = 'center'
 
 
 const vegList = document.getElementById('vegList');
@@ -17,6 +19,8 @@ vegList.style.listStyleType = 'none';
 vegList.style.fontSize = '20px'
 vegList.style.display = 'flex';
 vegList.style.gap = '20px';
+vegList.style.alignItems = 'center'
+vegList.style.justifyContent = 'center'
 
 
 const textTitle = document.getElementById('textTitle');
@@ -27,10 +31,25 @@ const fruitList = document.createElement('li');
 fruitList.textContent = 'Apple';
 fruList.appendChild(fruitList);
 
+fruitimg = document.createElement('img')
+fruitimg.src = 'https://producemadesimple.ca/wp-content/uploads/2023/08/apples-Ontario-fruits.jpg'
+fruitList.append(fruitimg);
+fruitimg.style.width = '220px';
+fruitimg.style.height = '245px';
+fruitimg.style.borderRadius = '25px';
+fruitimg.style.border = '3px solid red';
+
 const vegetableList = document.createElement('li');
 vegetableList.textContent = 'Bell pepper';
 vegList.appendChild(vegetableList);
 
+vegetableimg = document.createElement('img')
+vegetableimg.src = 'https://hips.hearstapps.com/hmg-prod/images/close-up-of-green-red-and-orange-bell-peppers-royalty-free-image-622418542-1558536290.jpg'
+vegetableList.append(vegetableimg);
+vegetableimg.style.width = '220px';
+vegetableimg.style.height = '220px';
+vegetableimg.style.borderRadius = '25px';
+vegetableimg.style.border = '2px solid green';
 
 
 const subtitle = document.getElementById('subtitle')
@@ -52,32 +71,37 @@ subheading.style.padding = '12px';
 
 const mango = document.getElementById('mango');
      mango.style.width = '220px';
+     mango.style.height = '245px';
      mango.style.borderRadius = '25px';
      mango.style.border = '2px solid red';
 
 const banana = document.getElementById('banana');
      banana.style.width = '200px';
+     banana.style.height = '245px';
      banana.style.borderRadius = '25px';
      banana.style.border = '2px solid red';
 
 const melon = document.getElementById('melon');
      melon.style.width = '200px';
+     melon.style.height = '245px';
      melon.style.borderRadius = '25px';
      melon.style.border = '2px solid red';
 
 const onion = document.getElementById('onion');
      onion.style.width = '220px';
+     onion.style.height = '245px';
      onion.style.borderRadius = '25px';
      onion.style.border = '2px solid green';
 
 const tomato = document.getElementById('tomato');
      tomato.style.width = '220px';
-     tomato.style.height = '170px';
+     tomato.style.height = '245px';
      tomato.style.borderRadius = '25px';
      tomato.style.border = '2px solid green';
 
 const kales = document.getElementById('kales');
      kales.style.width = '220px';
+     kales.style.height = '245px';
      kales.style.borderRadius = '25px';
      kales.style.border = '2px solid green';
 
